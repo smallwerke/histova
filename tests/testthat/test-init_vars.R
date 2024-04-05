b8e05d09f634277927bcc36d88292d7415e85168
smallwerke <6153030+smallwerke@users.noreply.github.com>
2024-04-05 12:33:52 -0500

    updated init_vars test

--- tests/testthat/test-init_vars.R
+++ tests/testthat/test-init_vars.R
@@ -3,9 +3,8 @@ the$MUTE = TRUE
 test_that("init_vars loads some default data", {
   init_vars()
   expect_equal(fig$Y.Min, 0)
-})
-
-test_that("init_vars loads some default data", {
-  init_vars()
   expect_equal(stats$Outlier, "TWO")
+  expect_equal(fig$Axis.X.Tick.Size, 0.6)
+  expect_equal(fig$Facet.Split, TRUE)
+  expect_equal(fig$Plot.Labels, "")
 })
